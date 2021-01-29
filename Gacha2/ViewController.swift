22abdde4d26d9d31e6e6c59e85ac1d484eedb2ac
sen <ochieeochieeochiee@gmail.com>
2021-01-29 19:52:08 +0900

    Initial Commit

---
+++ Gacha2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Gacha2
+//
+//  Created by Chihiro Nishiwaki on 2021/01/29.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
